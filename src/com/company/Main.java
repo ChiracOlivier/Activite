8c6759ea1042249cdb769af54ddf032362e1cf47
Chiracolivier <chiracolivier28@gmail.com>
2019-08-13 11:07:11 +0200

    creating Init class, InitOrdi class, ModeUnJoueur class, ModeDeuxJoueurs class, Choix class and fill them with a part of main class.

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,5 +5,14 @@ public class Main {
      * @param args
      */
     public static void main(String[] args) {
-      
+        int entree1;
+        String name1 = "Joueur 1";
+        entree1 = (new Choix()).choix(false, name1);
+        if (entree1 == 1) {
+            new ModeUnJoueur(name1);
+        } else if (entree1 == 2) {
+            new ModeDeuxJoueurs(name1);
+        }
+        System.out.println("FIN DU JEU");
+    }
 }

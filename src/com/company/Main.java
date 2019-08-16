c7aab27153363002f57aef66c088f4ca5dc21355
Chiracolivier <chiracolivier28@gmail.com>
2019-08-16 16:20:41 +0200

    Final arrange for every test for public classes and methods.

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -7,7 +7,13 @@ public class Main {
      */
     public static void main(String[] args) {
         int entree1;
-       
+        String name1 = "Joueur 1";
+        entree1 = (new Choix()).choix(false, name1);
+        if (entree1 == 1) {
+            new ModeUnJoueur(name1);
+        } else if (entree1 == 2) {
+            new ModeDeuxJoueurs(name1);
+        }
         System.out.println("FIN DU JEU");
     }
 }
